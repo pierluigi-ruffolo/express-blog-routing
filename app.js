0573359db7c0d0f01e390fa4525a944ff382e796
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-17 13:13:35 +0100

    feat: aggiungo Posts con immagini

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+import express from "express";
 
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Server avviato sulla porta" + " " + port);
+});
---
+++ comicPosts.js
@@ -0,0 +1,45 @@
+const comicPosts = [
+  {
+    titolo: "Il Ritorno del Cavaliere Oscuro: Un Capolavoro Rivoluzionario",
+    contenuto:
+      "L'opera di Frank Miller ha ridefinito il genere supereroistico, presentando un Batman anziano e disilluso in un futuro distopico. Un must-read per ogni appassionato.",
+    immagine: "/img/Cavaliere-Oscuro.png",
+    tags: ["Batman", "DC Comics", "Frank Miller", "Capolavoro", "Anni '80"],
+  },
+  {
+    titolo: "Watchmen: La Deconstruzione del Mito Supereroistico",
+    contenuto:
+      "Considerato uno dei romanzi grafici più importanti di sempre, Watchmen esplora temi di potere, moralità e politica con una narrazione complessa e personaggi profondi.",
+    immagine: "/img/One-Piece.png",
+    tags: [
+      "Alan Moore",
+      "DC Comics",
+      "Romanzo Grafico",
+      "Psicologico",
+      "Fantascientifico",
+    ],
+  },
+  {
+    titolo: "Spider-Man: Un Eroe con Problemi Reali",
+    contenuto:
+      "Ciò che ha reso Spider-Man così popolare è la sua umanità. Peter Parker non è solo un supereroe, ma anche uno studente che lotta con bollette, relazioni e responsabilità.",
+    immagine: "/img/Sandman.png",
+    tags: ["Marvel Comics", "Spider-Man", "Stan Lee", "Classico", "Supereroi"],
+  },
+  {
+    titolo: "Sandman: Il Mondo Onirico di Neil Gaiman",
+    contenuto:
+      "Una serie che mescola mitologia, storia e leggende in un universo affascinante incentrato su Sogno (Dream), uno degli Eterni. Una lettura ricca di riferimenti letterari.",
+    immagine: "/img/Spider-Man.png",
+    tags: ["Vertigo", "Neil Gaiman", "Fantasy", "Mitologia", "Onirico"],
+  },
+  {
+    titolo: "One Piece: L'Avventura Senza Fine",
+    contenuto:
+      "Il manga di Eiichiro Oda continua a battere record, seguendo la ciurma di Cappello di Paglia nella loro ricerca del tesoro One Piece. Un mix di azione, commedia ed emozione.",
+    immagine: "/img/Watchmen.png",
+    tags: ["Manga", "Shonen", "Eiichiro Oda", "Avventura", "Pirates"],
+  },
+];
+
+export default comicPosts;
